38a3b6d72948128aa8387a89a41e638d0cf0b398
dwinters <dwinters@mitre.org>
2017-03-27 10:39:56 -0400

    Support simple data transform to FHIR
    Introduce a simpler data format that can be transformed to the FHIR DSTU2 format required by the CDS.
    
    This is a combination of 23 commits:
    
    Initial class with default constructor parameters.
    
    Added bundle export method.
    
    Added method for writing pretty JSON to file.
    
    Added bookkeeping and formatting to keep track of resources as they're added to the bundle.
    
    Added resource specific methods to class.
    
    Simple test script.
    
    Simple test using a plain message for PatientBundle class.
    
    Removed broken factory.
    
    Refactored and put parse function into PatientBundle.js
    
    Added some comments.
    
    Added an check up front to see if the body had been parsed yet.
    
    Moved class to lib folder.
    
    Updated path to PatientBundle class.
    
    Removed meta field.
    
    Changed incorrect instances of Id to id.
    
    Removed name field.
    
    Fixed single-quote/double-quote issue, plus some other linting errors.
    
    Changed valueCodeableConcept to valueCoding
    
    Switched from taking in an age (and computing a fake birthdate), to
    taking in a birthdate.
    
    Changed class so patient resource ids do not have #-# format.
    
    Update signature for addObservation to better handle valueQuantity
    and valueCodeableConcept.
    
    Flattened the plain_message fixture into an array.
    
    Added some date input fields so they are not hard coded.
    Added a medication order to the simple message fixture.

---
+++ test/PatientBundle/test.js
@@ -0,0 +1,14 @@
+
+const pb = require('../../lib/PatientBundle');
+const message = require('./fixtures/plain_message.json');
+
+// Start a new patient bundle.
+var myPatient = new pb.PatientBundle();
+
+// Parse the message and append to myPatient.
+pb.parseMessage(message, myPatient);
+//pb.parseMessage(message, myPatient); // This also works
+
+// No test for now.  Just output to the screen.
+console.log(myPatient.getBundle());
+console.log(myPatient.getBundle().entry);
