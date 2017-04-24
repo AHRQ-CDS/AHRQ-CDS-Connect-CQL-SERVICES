834dc032646861ee34e44cc76925085b74796127
dwinters <dwinters@mitre.org>
2017-04-24 10:28:29 -0400

    Updated bodyParser options so that it will parse both 'application/json' and 'application/json+fhir' MIME content types.

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({type: function (msg)  {
+  if (msg.headers['content-type'] === 'application/json') {
+    return true;
+  } else if (msg.headers['content-type'] === 'application/json+fhir') {
+    return true;
+  } else {
+    return false;
+  }
+}}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
